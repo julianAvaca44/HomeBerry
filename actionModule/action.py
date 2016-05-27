5ae6fcac5d865fdd0b812b8f80ffa56d6cd2c6db
julianAvaca44 <julian.avaca44@gmail.com>
2016-05-27 01:42:12 -0300

    fix action.py

--- actionModule/action.py
+++ actionModule/action.py
@@ -2,15 +2,14 @@
 #es el encargado de comunicarse con las raspberry pi 3
 #caso de algun fallo comunicar lo
 # la idea de cuando llega un comando que analize un objecto con la siguiente estructura
-"""
+
 listCommand = {
-	'nameFunc_1': func_1,
-	'nameFunc_2': func_2,
-	'nameFunc_3': func_3,
-	'nameFunc_4': func_4,
+	'encender': func_1,
+	'apagar': func_2,
+	'activar': func_3,
+	'desactivar': func_4,
 }
 
-"""
 #luego ejecutar la funcion, algo asi
 #listCommand.[command]() revisar y buscar la manera lo estoy escribiendo a medida q se me ocurre pero la idea esta jajaj
 
@@ -22,5 +21,10 @@ log.basicConfig(filename='./action.log', filemode='w', level=log.DEBUG)
 def acction(command):
 	if command == None:
 		return "comando no valido"	
-	elif command[0] == 'encender':
+	elif listCommand(command[0]) == 'encender':
 		return "encender"
+
+		#if command[0] in listCommand.keys(): 
+		#	listCommand[command[0]]()
+		#else:
+		#	return "No se reconoce el commando" 
