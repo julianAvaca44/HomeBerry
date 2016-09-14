92053e169ff6585ad26f3f3e6d911b8ba42054aa
julianAvaca44 <julian.avaca44@gmail.com>
2016-09-14 02:26:19 +0000

    add constante.py

---
+++ constantes.py
@@ -0,0 +1,28 @@
+
+#(comando)(dispositivo)[(numero de dispositivo)(area)|(area)|(numero de dispositivo)]
+REGEXCOMMANDS = "^\s*((\w+)\s(\w+)(\s(\w+)\s(\w+)|\s(\w+))?).*$"
+TOKEN = "207063163:AAH5GbFmvmNnNog6ZJ0QXI1ZI2KFX0rsxaI"
+TELEFONO = "5491169368980"
+PASSWORD = "hBTEFCYv2Wc5ZtICZ4H7VReH5sk="
+ 
+#variables >
+DISP_LUCES = "luces"
+DISP_LUZ = "luz"
+DISP_ALARMA = "alarma"
+DISP_SENSOR = "sensor"
+DISP_VENTILADOR = "ventilador"
+DISP_INEXISTENTES = "Dispositivo inexistente"
+DISP_ON = "Dispositivo encendido"
+DISP_OFF = "Dispositivo apagado"
+DISP_ON_PLUS = "Dispositivos encendidos"
+DISP_OFF_PLUS = "Dispositivos apagados"
+DISP_ACTIVADO_ALARMA = "Alarma activada"
+DISP_DESACTIVADO_ALARMA = "Alarma desactivado"
+DISP_ALARMA_DETECTAR = "Intruso detectado"
+COMMAND_ENCENDER = "enceder"
+COMMAND_APAGAR = "apagar"
+COMMAND_ACTIVAR = "activar"
+COMMAND_APAGAR = "desactivar"
+COMMAND_INVALID = "comando invalido"
+COMMAND_UNRECOGNIZABLE = "No se reconoce el comando"
+ 
